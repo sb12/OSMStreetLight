f5a9c7bf577dd036fed90cb886c4d411dd1af5b2
sb12 <osm.mapper999@gmail.com>
2018-12-22 20:12:11 +0100

    show current language

--- js/i18nextInit.js
+++ js/i18nextInit.js
@@ -23,6 +23,7 @@ function updateContent() {
 	document.getElementById("aboutmap").innerHTML = i18next.t('aboutmap');
 	document.getElementById("zoomin").innerHTML = i18next.t('zoomin');
 	document.title = i18next.t('website_title');
+	document.getElementById("langselect").value = i18next.language;
 }
 
 function changeLng(lng) {
