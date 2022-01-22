4a31b1300cf8d69980ce76f47afdeb7a949460d4
Михаил <41448637+mkgrgis@users.noreply.github.com>
2022-01-22 20:21:17 +0300

    leaflet.js: Russische Übersetzung / RU translation

--- leaflet/plugins/LanguageSelector/LanguageSelector.js
+++ leaflet/plugins/LanguageSelector/LanguageSelector.js
@@ -7,7 +7,12 @@ L.Control.LanguageSelector = L.Control.extend({
     onAdd: function () {
         
         var language_selector_div = L.DomUtil.create('div', 'leaflet-control-layers');
-        language_selector_div.innerHTML = '<select id="langselect"><option onclick="i18next.changeLanguage(\'en\')" value="en">EN </option><option onclick="i18next.changeLanguage(\'de\')" value="de">DE </option><option onclick="i18next.changeLanguage(\'nl\')" value="nl">NL </option></select>';
+        language_selector_div.innerHTML = '<select id="langselect">'+
+            '<option onclick="i18next.changeLanguage(\'en\')" value="en">EN </option>'+
+            '<option onclick="i18next.changeLanguage(\'de\')" value="de">DE </option>'+
+            '<option onclick="i18next.changeLanguage(\'nl\')" value="nl">NL </option>'+
+            '<option onclick="i18next.changeLanguage(\'ru\')" value="ru">RU </option>'+
+            '</select>';
         $(language_selector_div).attr('id', 'lang');
         
         return language_selector_div;
