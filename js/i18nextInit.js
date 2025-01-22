60f5cceeaf83bf28db8447feb37284e98fe125e7
sb12 <sb12@users.noreply.github.com>
2025-01-22 11:33:49 +0100

    Fix translation for lowzoom data loading request

--- js/i18nextInit.js
+++ js/i18nextInit.js
@@ -6,7 +6,7 @@ i18next
   .init({
     fallbackLng: 'en',
     whitelist: ['de','en','ja','nl','ru'],
-    debug: true,
+    debug: false,
     detection: {
       order: ['cookie', 'localStorage'],
       lookupCookie: 'next-i18next',
@@ -23,16 +23,23 @@ i18next
 // just set some content and react to language changes
 // could be optimized using vue-i18next, jquery-i18next, react-i18next, ...
 function updateContent() {
+  
   $("#aboutmap").html(i18next.t('aboutmap'));
   document.title = i18next.t('website_title');
   $("#langselect").val(i18next.language);
   $("#opacity_slider").attr('title', i18next.t("opacity_select"));
   $("#lang").attr('title', i18next.t("lang_select"));
+  
   $("#layer_street_lights_lowzoom").html(i18next.t("layer_street_lights_lowzoom"));
   $("#layer_street_lights").html(i18next.t("layer_street_lights"));
   $("#layer_aviation_lights").html(i18next.t("layer_aviation"));
   $("#layer_lit_streets").html(i18next.t("layer_lit_streets"));
   $("#layer_unlit_streets").html(i18next.t("layer_unlit_streets"));
+  
+  $("#update_lowzoom_data").html(i18next.t("update_lowzoom_data"));
+  $("#load_lowzoom_data").html(i18next.t("load_lowzoom_data"));
+  $("#clear_lowzoom_data").html(i18next.t("clear_lowzoom_data"));
+  
   MoveCall(5);
   console.log("ContentUpdated");
 }
